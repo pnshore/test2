6f90b99bd68a3a0be57e870b7d04b0f86c8b1356
pnshore <pshore@novetta.com>
2016-05-11 20:35:54 +0000

    testing another file

---
+++ testingMore.go
@@ -0,0 +1,19 @@
+project main
+
+import "fmt"
+
+
+type I interface{
+  methodCall()
+}
+
+type t strut{
+  X String
+}
+
+func (t strutTest) methodCall() string{
+    return t.X
+}
+func main(){
+
+  fmt.Println(methodCall)
