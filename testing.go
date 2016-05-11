33cfcd808030eefc183e8f9802259b6bbf5b281c
pnshore <pshore@novetta.com>
2016-05-11 19:39:30 +0000

    third github test

--- testing.go
+++ testing.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type structTest struct {
+	X int
+	Y int
+}
+
+func (s structTest) methodTest() int {
+	return s.X * s.Y
+}
+
 func main() {
-	fmt.Println("hey! this is a test")
+	v := structTest{3, 9}
+	fmt.Println(v.methodTest())
 }
