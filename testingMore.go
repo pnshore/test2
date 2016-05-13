7bd553d79d5ce32bda31d10c5d6bd92e26dd3055
pnshore <pshore@novetta.com>
2016-05-13 16:46:52 +0000

    created a better test

--- testingMore.go
+++ testingMore.go
@@ -1,19 +1,29 @@
-project main
+package main
 
 import "fmt"
 
+var m map[string]int
 
-type I interface{
-  methodCall()
+func main() {
+
+	var interT interface{} = 234
+	s, je := interT.(string)
+	fmt.Println(s, je)
+
+	m = make(map[string]int)
+	m["mapTest"] = 40
+	fmt.Println(m["mapTest"])
+	i, ok := m["mapTest"]
+	fmt.Println(i, ok)
+	y := []int{1, 2, 5, 90}
+	for i := 0; i < len(y); i++ {
+		fmt.Println(y[i])
+	}
+	commits := map[string]int{
+		"rsc": 234,
+		"r":   23,
+		"sf":  45634,
+	}
+
+	fmt.Println(commits["rsc"])
 }
-
-type t strut{
-  X String
-}
-
-func (t strutTest) methodCall() string{
-    return t.X
-}
-func main(){
-
-  fmt.Println(methodCall)
